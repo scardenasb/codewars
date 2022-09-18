7919f9659ee2dc3f84fe8493045c06e33d4bc179
scardenasb <84429399+scardenasb@users.noreply.github.com>
2022-09-18 04:48:00 -0300

    update

--- python3/weight_for_weight.py
+++ python3/weight_for_weight.py
@@ -1,18 +1,5 @@
-from functools import reduce
-
-
 def order_weight(st):
     list_st = sorted(st.split())
-    res = []
-    minimus = list(
-        map(lambda x: reduce(lambda i, j: int(i) + int(j), x), list_st)
-    )
-    minimus = list(map(int, minimus))
-
-    for i in range(len(list_st)):
-        list_mini = list_st[minimus.index(min(minimus))]
-        res.append(list_mini)
-        list_st.remove(list_mini)
-        minimus.remove(min(minimus))
+    res = sorted(list_st, key=lambda x: sum(int(i) for i in x))
 
     return ' '.join(res)
